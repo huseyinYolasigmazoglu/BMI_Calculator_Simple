65a96dac47f6999df32be7033d8c546b2aad5dbc
huseyinYolasigmazoglu <huseyinyolasigmazoglu@hotmail.com>
2020-12-01 14:48:47 +0300

    First checkIn

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Huseyin Yolasigmazoglu on 29/08/2020.
+//  Copyright © 2020 Huseyin Yolasigmazoglu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
